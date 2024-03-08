1a02a53925372bb3c8a08ccca4515ddc9710dd46
ParthDabholkar <parth.dabholkar@gmail.com>
2024-03-08 16:49:59 +0530

    Connected to MongoDB Database

--- serverSide/index.js
+++ serverSide/index.js
@@ -1,5 +1,16 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+mongoose.connect(process.env.MONGO)
+.then(() => {
+    console.log("MongoDB is connected")
+}).catch((err) => {
+    console.log(err)
+})
 
 app.listen(3000, () => {
     console.log("Listening on PORT 3000!!!")
